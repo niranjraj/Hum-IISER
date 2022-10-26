ac72fd0f23a265d3a430b66683fd7ec02f2edeb4
niranjRaj <niranjrajesh7007@gmail.com>
2022-10-26 08:31:15 +0530

    downgrade next

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    SECRET: process.env.SECRET,
+    SHADOW_DATABASE_URL: process.env.SHADOW_DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+  },
 };
 
 module.exports = nextConfig;
